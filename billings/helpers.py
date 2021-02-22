6b4194d565c7b472b3dd6fb312f0c8863c539d7f
Muhaafidz Md Saufi <muhaafidz@pipeline.com.my>
2021-02-22 12:25:59 +0800

    update django admin billing

--- billings/helpers.py
+++ billings/helpers.py
@@ -23,22 +23,24 @@ def payment_response_process(request):
     if status == '1' or status == '0':
         order_id = request.POST['OrderID']
         status_desc = request.POST['StatusDesc']
-        transaction_date = request.POST['TransactionDate']
-        transaction_id = request.POST['TransactionID']
-        payment_method_desc = request.POST['PaymentMethodDesc']
-        payment_method = request.POST['PaymentMethod']
-        auth_code = request.POST['AuthCode']
-        receipt_no = request.POST['ReceiptNo']
+        if status == '1':
+            transaction_date = request.POST['TransactionDate']
+            transaction_id = request.POST['TransactionID']
+            payment_method_desc = request.POST['PaymentMethodDesc']
+            payment_method = request.POST['PaymentMethod']
+            auth_code = request.POST['AuthCode']
+            receipt_no = request.POST['ReceiptNo']
         
         payment, created = Payment.objects.get_or_create(order_id=order_id)
         payment.payment_status = int(status)
-        payment.payment_method = payment_method
-        payment.payment_method_description = payment_method_desc
-        payment.payment_date = datetime.strptime(transaction_date, "%d/%m/%Y %H:%M:%S")
-        payment.transaction_id = transaction_id
-        payment.auth_code = auth_code
-        payment.receipt_number = receipt_no
         payment.status_description = status_desc
+        if status == '1':
+            payment.payment_method = payment_method
+            payment.payment_method_description = payment_method_desc
+            payment.payment_date = datetime.strptime(transaction_date, "%d/%m/%Y %H:%M:%S")
+            payment.transaction_id = transaction_id
+            payment.auth_code = auth_code
+            payment.receipt_number = receipt_no
         payment.save()
 
         return payment
