c2af780c123dfd1ed863d52d2278374581950d60
Muhaafidz Md Saufi <muhaafidz@pipeline.com.my>
2021-02-23 13:57:07 +0800

    test

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -23,7 +23,7 @@ def docx_to_pdf_process_stream(letter_template, context, download):
     # Method 5
     # template_path = requests.get(absoluteuri.build_absolute_uri(letter_template.template_file.url))
 
-    template_path = os.path.join(settings.BASE_DIR,settings.MEDIA_ROOT,letter_template.template_file.name)
+    template_path = os.path.join(settings.MEDIA_ROOT,letter_template.template_file.name)
     print(template_path)
     # Method 3
     # template_path = absoluteuri.build_absolute_uri(letter_template.template_file.url)
