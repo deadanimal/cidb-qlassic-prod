f17b633045f68c1430cfdd9ea380fecebac7968f
Muhaafidz Md Saufi <muhaafidz@pipeline.com.my>
2021-02-24 11:27:03 +0800

    add job. remove tmp folder

--- api/jobs.py
+++ api/jobs.py
@@ -1,5 +1,8 @@
 import shutil
+import os
+
 from app.helpers.docx2pdf import TEMPLATE_TEMP_PATH
 def job_remove_temp():
-    shutil.rmtree(TEMPLATE_TEMP_PATH) 
-    print('JOB: Clear up tmp folder')
+    if os.path.exists(TEMPLATE_TEMP_PATH):
+        shutil.rmtree(TEMPLATE_TEMP_PATH) 
+        print('JOB: Clear up tmp folder')
