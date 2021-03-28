63da1228d037dd5db15033dec65eb622468e4c32
Muhaafidz Md Saufi <muhaafidz@pipeline.com.my>
2021-03-28 21:00:20 +0800

    fix url

--- api/urls.py
+++ api/urls.py
@@ -63,16 +63,16 @@ assessors_router = router.register(
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # REST
-    url('api/v1/isAlive', IsAliveView.as_view(), name='isAlive'),
-    url('api/v1/readySync/<str:id>', ReadySyncView.as_view(), name='readySync'),
-    url('api/v1/readyComplete/<str:id>', ReadyCompleteView.as_view(), name='readyComplete'),
+    path('api/v1/isAlive', IsAliveView.as_view(), name='isAlive'),
+    path('api/v1/readySync/<str:id>', ReadySyncView.as_view(), name='readySync'),
+    path('api/v1/readyComplete/<str:id>', ReadyCompleteView.as_view(), name='readyComplete'),
     path('api/v1/getDocument/<str:id>', GetDocumentView.as_view(), name='getDocument'),
-    url('api/v1/getProjectData', GetProjectDataView.as_view(), name='api_get_project_data'),
-    url('api/v1/sync', SyncView.as_view(), name='api_sync'),
-    url('api/v1/overview/<str:projectID>', OverviewView.as_view(), name='api_overview'),
-    url('api/v1/complete', CompleteView.as_view(), name='api_complete'),
-    url('api/v1/getAgreement/<str:projectID>', GetAgreementView.as_view(), name='api_getAgreement'),
-    url('api/v1/appNo/<str:projectID>', AppNoView.as_view(), name='api_appNo'),
+    path('api/v1/getProjectData', GetProjectDataView.as_view(), name='api_get_project_data'),
+    path('api/v1/sync', SyncView.as_view(), name='api_sync'),
+    path('api/v1/overview/<str:projectID>', OverviewView.as_view(), name='api_overview'),
+    path('api/v1/complete', CompleteView.as_view(), name='api_complete'),
+    path('api/v1/getAgreement/<str:projectID>', GetAgreementView.as_view(), name='api_getAgreement'),
+    path('api/v1/appNo/<str:projectID>', AppNoView.as_view(), name='api_appNo'),
     
     url(r'api/v1/', include(router.urls)),
     url(r'auth/', include('rest_auth.urls')),
