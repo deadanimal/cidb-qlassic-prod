e8a60faef7c8a66ab57fb27c9ff8ddfa84f57409
quantmis <realhost01@gmail.com>
2021-05-07 12:29:54 +0800

    updateee

--- app/views.py
+++ app/views.py
@@ -1263,7 +1263,7 @@ def assessment_report_generate(request, report_type, qaa):
             'grade': qaa.pi.contractor_registration_grade,
             'ccd_score': str(round(qaa.ccd_point, 2)),
             'qlassic_score': rounded_qlassic_score,
-            'scope': json.loads(scope)
+            'scope': json.loads(scope.scope)
 
         }
         response_cert = generate_document_file(request, report_type, template_ctx, reporting.qr_file)
