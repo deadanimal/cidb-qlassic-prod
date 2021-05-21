1b982e72e8fb0e0e0bc30e22fb4775b6926da712
quantmis <realhost01@gmail.com>
2021-05-21 11:31:57 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1200,7 +1200,7 @@ def assessment_report_detail_ef(request, id):
                         sub.append(e.result)
 
 
-            sub.append(f"<a class='btn btn-sm btn-default mb-2' href={s.photo_1}>1</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_2}>2</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_3}>3</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_4}>4</a>")
+            sub.append(f"<a class='btn btn-sm btn-default mb-2' href={s.photo_1.url}>1</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_2.url}>2</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_3.url}>3</a>&nbsp<a class='btn btn-sm btn-default mb-2' href={s.photo_4.url}>4</a>")
         
             column_results.append(sub)
 
