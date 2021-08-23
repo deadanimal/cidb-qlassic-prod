a376278ffe2d093215dacc7af612b5b46222fe1d
quantmis <realhost01@gmail.com>
2021-08-23 17:24:02 +0800

    fix path and rename

--- app/views.py
+++ app/views.py
@@ -1444,7 +1444,7 @@ def assessment_report_generate(request, report_type, qaa):
             'qlassic_score': rounded_qlassic_score,
         }
         response_cert = generate_document_file(request, report_type, template_ctx, reporting.qr_file)
-        reporting.report_file.save(f"{qaa.id}_{report_type}.pdf", response_cert)
+        reporting.report_file.save(f"{qaa.id}_{report_type}", response_cert)
     elif report_type == 'qlassic_report':
         qaa_result = get_qaa_result(qaa)
         assessors = AssignedAssessor.objects.all().filter(ad__qaa=qaa)
@@ -1482,7 +1482,7 @@ def assessment_report_generate(request, report_type, qaa):
         response_cert = generate_document_file(request, report_type, template_ctx, None)
 
         #reporting.report_file.save(reporting.report_file, response_cert)
-        reporting.report_file.save(f"{qaa.id}_{report_type}.pdf", response_cert)
+        reporting.report_file.save(f"{qaa.id}_{report_type}", response_cert)
 
 
     elif report_type == 'qlassic_certificate':
@@ -1505,7 +1505,7 @@ def assessment_report_generate(request, report_type, qaa):
         }
         response_cert = generate_document_file(request, report_type, template_ctx, reporting.qr_file)
         #reporting.report_file.save(reporting.report_file, response_cert)
-        reporting.report_file.save(f"{qaa.id}_{report_type}.pdf", response_cert)
+        reporting.report_file.save(f"{qaa.id}_{report_type}", response_cert)
 
     else:
         return None
